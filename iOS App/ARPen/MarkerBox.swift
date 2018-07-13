011599a04b042956c56e6d1c2a43609c56742c5e
Oliver Nowak <>
2018-07-13 14:37:55 +0200

    both landscape orientations are working

--- iOS App/ARPen/MarkerBox.swift
+++ iOS App/ARPen/MarkerBox.swift
@@ -14,6 +14,29 @@ import SceneKit
 class MarkerBox: SCNNode {
     
     private var markerArray: [SCNNode]
+    var penLength: Double = 12
+    
+    /**
+     * Describes in which landscape orientation the device is currently hold
+     * If the device is hold in portrait orientation, the state keeps in the last landscape mode
+     */
+    private var orientationState: DeviceOrientationState = .HomeButtonRight {
+        didSet {
+            if orientationState == .HomeButtonLeft {
+                reverseXY = -1
+            } else if orientationState == .HomeButtonRight {
+                reverseXY = 1
+            }
+            //For each orientation the pen tip has to be calculated
+            calculatePenTip(length: penLength)
+        }
+    }
+    
+    
+    /// Factor for marker x and y coordinates. If the device is rotated, the coordinates of the aruco marker must be inverted, which is done by multiplying with this variable
+    
+    /// This must be either 1 or -1!
+    private var reverseXY: Float = 1
     
     override convenience init() {
         self.init(length: UserDefaults.standard.double(forKey: UserDefaultsKeys.penLength.rawValue))
@@ -21,25 +44,43 @@ class MarkerBox: SCNNode {
     
     init(length: Double) {
         markerArray = [SCNNode(), SCNNode(), SCNNode(), SCNNode(), SCNNode(), SCNNode()]
+        penLength = length
         super.init()
         self.name = "MarkerBox"
+        
+        //Observe device orientation. If orientaion changed rotated() is called
+        NotificationCenter.default.addObserver(self, selector: #selector(rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+        
+        //Make pen tip calculation
+        calculatePenTip(length: length)
+    }
+    
+    @objc func rotated(){
+        if UIDevice.current.orientation.rawValue == 4 {
+            orientationState = .HomeButtonLeft
+        } else if UIDevice.current.orientation.rawValue == 3 {
+            orientationState = .HomeButtonRight
+        }
+    }
+    
+    func calculatePenTip(length: Double){
         
         let a: Double = length
         var xs, ys, zs, xl, yl, zl: Double
         
-        let angle = 35.3.degreesToRadians
+        let angle = (35.3).degreesToRadians
         
-        xs = ((cos(angle)*a)+0.005)/sqrt(2)
+        xs = ((cos(angle) * a) + 0.005)/sqrt(2)
         ys = xs
-        zs = sin(angle)*a
+        zs = sin(angle) * a
         zs -= 0.02
         xs *= -1
         ys *= -1
         zs *= -1
         
-        xl = (cos(angle)*a)/sqrt(2)
+        xl = (cos(angle) * a)/sqrt(2)
         yl = xl
-        zl = sin(angle)*a
+        zl = sin(angle) * a
         zl += 0.02
         xl *= -1
         yl *= -1
@@ -47,6 +88,7 @@ class MarkerBox: SCNNode {
         var i = 0
         for marker in markerArray {
             marker.name = "Marker #\(i+1)"
+            marker.childNodes.first?.removeFromParentNode()
             let point = SCNNode()
             point.name = "Point from #\(i+1)"
             
@@ -67,12 +109,18 @@ class MarkerBox: SCNNode {
                 break
             }
             
+            //Rotate the coordinates if landscape oriention needs that
+            point.position.x *= reverseXY
+            point.position.y *= reverseXY
+            
+            
             marker.addChildNode(point)
-            self.addChildNode(marker)
+            if !self.childNodes.contains(marker){
+                self.addChildNode(marker)
+            }
             
             i += 1
         }
-        
     }
     
     /**
@@ -81,6 +129,13 @@ class MarkerBox: SCNNode {
     func set(position: SCNVector3, rotation: SCNVector3, forID id: Int) {
         self.markerArray[id-1].position = position
         self.markerArray[id-1].eulerAngles = rotation
+        
+        //If orientation is LandscapeRight we have to revert x and y axis and marker orientation
+        self.markerArray[id-1].position.x *= reverseXY
+        self.markerArray[id-1].position.y *= reverseXY
+        
+        self.markerArray[id-1].eulerAngles.x *= reverseXY
+        self.markerArray[id-1].eulerAngles.y *= reverseXY
     }
     
     /**
@@ -102,5 +157,8 @@ class MarkerBox: SCNNode {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    private enum DeviceOrientationState {
+        case HomeButtonLeft
+        case HomeButtonRight
+    }
 }
