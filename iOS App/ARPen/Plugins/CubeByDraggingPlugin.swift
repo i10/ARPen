54f6abcaafaedb0624c54ec5be9448b8007194cb
imkrishsub <krishna@cs.rwth-aachen.de>
2019-06-13 09:52:07 +0200

    Add instructions for sphere and cube plugins

--- iOS App/ARPen/Plugins/CubeByDraggingPlugin.swift
+++ iOS App/ARPen/Plugins/CubeByDraggingPlugin.swift
@@ -15,7 +15,7 @@ class CubeByDraggingPlugin: Plugin, UserStudyRecordPluginProtocol {
     var recordManager: UserStudyRecordManager!
     
     var pluginImage : UIImage? = UIImage.init(named: "CubeByDraggingPlugin")
-    var pluginInstructionsImage: UIImage? = UIImage.init(named: "PaintPluginInstructions")
+    var pluginInstructionsImage: UIImage? = UIImage.init(named: "CubePluginInstructions")
     var pluginIdentifier: String = "Cube"
     var needsBluetoothARPen: Bool = false
     var pluginDisabledImage: UIImage? = UIImage.init(named: "ARMenusPluginDisabled")
